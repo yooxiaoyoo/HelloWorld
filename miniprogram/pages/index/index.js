9b446e49c128114dd21301a3ad8a2152285b110e
悠小呦 <18843188782@163.com>
2021-05-25 10:32:05 +0800

    feat:0525练习，添加弹窗组件;

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,8 +7,24 @@ Page({
    */
   data: {
     openid:"",
+    nbFrontColor: '#000000',
+    nbBackgroundColor: '#ffffff',
+    list: [{
+      "text": "对话",
+      "iconPath": "../../images/tabbar_icon_chat_default.png",
+      "selectedIconPath": "../../images/tabbar_icon_chat_active.png",
+        dot: true
+    },
+    {
+        "text": "设置",
+      "iconPath": "../../images/tabbar_icon_setting_default.png",
+      "selectedIconPath": "../../images/tabbar_icon_setting_active.png",
+        badge: 'New'
+    }]
   },
-
+  tabChange(e) {
+    console.log('tab change', e)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -35,7 +51,31 @@ Page({
         }
       })
     })
+    // 页面停留3s后跳转(定时器功能)
+    // setTimeout(function () {
+    //   wx.reLaunch({
+    //     url: '/pages/page1-home/index',
+    //   })
+    // }, 3000);
+    //获得dialog组件
+    this.dialog = this.selectComponent("#dialog");
   },
+
+  // 弹窗组件部分
+  showDialog(){
+    this.dialog.showDialog();
+  },
+  //取消事件
+  _cancelEvent(){
+    console.log('你点击了取消');
+    this.dialog.hideDialog();
+  },
+  //确认事件
+  _confirmEvent(){
+    console.log('你点击了确定');
+    this.dialog.hideDialog();
+  },
+  // 弹窗组件部分结束
 
   /**
    * 生命周期函数--监听页面初次渲染完成
